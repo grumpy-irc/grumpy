8643be38a682e997a40c5f5bbede86664ea17778
Petr Bena <benapetr@gmail.com>
2018-04-02 01:29:04 +0200

    install

--- travis/install.sh
+++ travis/install.sh
@@ -4,9 +4,9 @@
 #	sudo apt-get install -y libqt4-webkit libqtwebkit-dev qt4-qmake qt4-dev-tools cmake
 #fi
 
-#if [ "$QTTYPE" = "5" ]; then
-#	sudo apt-get install -y ubuntu-sdk qtquick1-5-dev cmake
-#fi
+if [ "$QTTYPE" = "5" ]; then
+	sudo apt-get install -y qtbase5-dev qtdeclarative5-dev qt5-default qttools5-dev-tools
+fi
 
 #sudo apt-get install g++-4.7
 #sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 20 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
