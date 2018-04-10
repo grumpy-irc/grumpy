ff43ab94fd49de69505c7d550e3247dbc5b7c42a
Petr Bena <benapetr@gmail.com>
2018-04-10 23:28:09 +0200

    snappy

--- src/tools/unpack.sh
+++ src/tools/unpack.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pwd
-if [ ! -f "sqlite/sqlite3.h" ];then
-    cd "sqlite" || exit 1
+if [ ! -f "../src/sqlite/sqlite3.h" ];then
+    cd "../src/sqlite" || exit 1
     unzip sqlite-amalgamation-3220000.zip || exit 1
     mv sqlite-amalgamation-3220000/* . || exit 1
     rm -rf sqlite-amalgamation-3220000 || exit 1
